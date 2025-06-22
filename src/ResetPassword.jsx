4e44bf9c9bb2b0a7ea71f6e8364b4cee4e6331d0
Prashant <rnttl90@gmail.com>
2025-06-22 23:02:55 +0530

    a

--- src/ResetPassword.jsx
+++ src/ResetPassword.jsx
@@ -163,6 +163,7 @@ function ResetPassword() {
     const [newPassword, setNewPassword] = useState("");
     const [message, setMessage] = useState("");
     const [error, setError] = useState("");
+    console.log(oobCode)
 
     const handleResetPassword = async (e) => {
       e.preventDefault();
