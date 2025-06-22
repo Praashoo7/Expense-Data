3965c6eab2606ce319bd7579b0bc1f22066ebeb3
Prashant <rnttl90@gmail.com>
2025-06-22 22:10:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import Login from "./Login.jsx";
 import SignUp from "./SignUp.jsx";
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <HashRouter basename="/">
       <Routes>
         <Route
           path="/Expense-Data/:username"
@@ -61,7 +61,7 @@ function App() {
           element={<Navigate to={isLoggedIn ? `/Expense-Data/${localStorage.getItem("loggedInUsername")}` : "/Expense-Data/NotFound"} />}
         />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
